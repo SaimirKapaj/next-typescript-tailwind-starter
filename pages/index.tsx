03548ab39631d5c4e05da7a1b0cc80a9fcc04b3d
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-08-06 16:51:15 +0200

    changed project structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,11 @@
-import Head from 'next/head';
+import Layout from 'components/Layout';
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <div>
-      <Head>
-        <title>Next-Typescript-Tailwind-Starter</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="max-w-screen-xl mx-auto p-4 text-center">
-        <h1 className="text-xl">
-          Welcome to <span className="text-blue-600">Next-Typescript-Tailwind-Starter</span>
-        </h1>
-      </main>
-    </div>
+    <Layout>
+      <h1 className="text-xl">Main Content</h1>
+    </Layout>
   );
-}
+};
+
+export default HomePage;
