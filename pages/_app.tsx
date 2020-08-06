8bb6f1beb6e5e324493742ad5e076b9a095717b5
Saimir Kapaj <saimir.kapaj@pm-international.com>
2020-08-06 16:09:21 +0200

    installed tailwind css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import 'styles/tailwind.base.css';
+import 'styles/base.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
